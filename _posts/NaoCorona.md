c504f70c3410b210fe05941e1bf028e91b919ca2
João Henrique Riboli <joao.riboli@outlook.com>
2020-06-19 14:48:58 -0300

    initial commit

---
+++ _posts/NaoCorona.md
@@ -0,0 +1,6 @@
+---
+layout: post
+title:  "Não é Corona, é só rinite"
+spotify: spotify:playlist:5Uq1CfOEsf1D2UbxEMnv7n
+---
+
---
+++ npmfile.js
@@ -0,0 +1,3 @@
+exports.printMsg = function() {
+    console.log("I'm so sad, please follow all my playlists on spotify. :(");
+};
